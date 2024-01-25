bee9faf31b37b71c78fc0e50198de98d5a5b49f2
Siya Kamboj <siyakamboj20@gmail.com>
2024-01-25 10:32:43 -0800

    Changed ingestion code; tried to write code that used diff

---
+++ JSON/Ingesting/siya/comparendjson.py
@@ -0,0 +1,37 @@
+import ndjson
+import json
+import difflib
+
+def compare_ndjson_files(file1_path, file2_path):
+    # Read the contents of the NDJSON files
+    with open(file1_path, 'r') as file1:
+        #ndjson_content1 = ndjson.load(file1)
+        ndjson_content1 = [json.loads(line) for line in file1]
+        #text1 = file1.readlines()
+        
+    with open(file2_path, 'r') as file2:
+        ndjson_content2 = ndjson.load(file2)
+        #ndjson_content2 = [json.loads(line) for line in file2]
+        #ndjson_content2 = json.loads(file2.read())
+        #text2 = file2.readlines()
+
+    # Compute the diff
+    differ = difflib.Differ()
+    diff = list(differ.compare(ndjson.dumps(ndjson_content1, default=str).splitlines(), ndjson.dumps(ndjson_content2, default=str).splitlines()))
+
+    # Print or process the differences
+    for line in diff:
+        print(line)
+
+
+    # Print or process the differences
+    for line in diff:
+        print(line)
+
+# Example usage
+official_hetero_file="../../../DataSet/heterogeneous.ndjson"
+official_homo_file="../../../DataSet/homogeneous.ndjson"
+
+siya_hetero_file="siya_hetero_data.ndjson"
+siya_homo_file="siya_homo_data.ndjson"
+compare_ndjson_files(official_homo_file, siya_homo_file)
--- JSON/Ingesting/siya/siya.py
+++ JSON/Ingesting/siya/siya.py
@@ -2,6 +2,9 @@ import json
 hetero_file="../../../DataSet/heterogeneous.ndjson"
 homo_file="../../../DataSet/homogeneous.ndjson"
 
+homo_file_half="../../../DataSet/homogeneous_half.ndjson"
+hetero_file_half="../../../DataSet/heterogeneous_half.ndjson"
+
 with open(homo_file, "r") as f:
     #save it line by line
     data = [json.loads(l) for l in f.readlines()]
