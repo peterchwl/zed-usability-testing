14e8173b21a85059b8ff384ae505c68d3eafedbb
Siya Kamboj <siyakamboj20@gmail.com>
2024-01-25 16:46:02 -0800

    Siya update

--- JSON/Ingesting/siya/siya.py
+++ JSON/Ingesting/siya/siya.py
@@ -2,6 +2,7 @@ import ndjson
 hetero_file="../../../DataSet/heterogeneous.ndjson"
 homo_file="../../../DataSet/homogeneous.ndjson"
 
+data=[]
 with open(homo_file, "r") as f:
     data = [ndjson.loads(l) for l in f.readlines()]
 with open("siya_homo_data.ndjson", "w") as f:
@@ -9,6 +10,7 @@ with open("siya_homo_data.ndjson", "w") as f:
         ndjson.dump(x, f)
         f.write('\n')
 
+data=[]
 with open(hetero_file, "r") as f:
     data = [ndjson.loads(l) for l in f.readlines()]
 with open("siya_hetero_data.ndjson", "w") as f:
--- JSON/Querying/siya/siyaJSONQuerying.sh
+++ JSON/Querying/siya/siyaJSONQuerying.sh
@@ -19,22 +19,22 @@ while true; do
 done
 
 echo "Search query 1"
-jq 'select(.ts=="2018-03-24T17:15:21.196410Z")' $FILE > $typeoffile"geneous_results/search1.txt"
+jq -c 'select(.ts=="2018-03-24T17:15:21.196410Z")' $FILE > $typeoffile"geneous_results/search1.txt"
 
 echo "Search query 2"
-jq 'select(._path=="http")' $FILE > $typeoffile"geneous_results/search2.txt"
+jq -c 'select(._path=="http")' $FILE > $typeoffile"geneous_results/search2.txt"
 
 echo "Search query 3"
-jq 'select(."id.orig_h"=="10.47.5.155")' $FILE > $typeoffile"geneous_results/search3.txt"
+jq -c 'select(."id.orig_h"=="10.47.5.155")' $FILE > $typeoffile"geneous_results/search3.txt"
 
 echo "Analytical query 1"
-jq '._path' $FILE | sort -u > $typeoffile"geneous_results/analytical1.txt"
+jq -r '._path' $FILE | sort -u > $typeoffile"geneous_results/analytical1.txt"
 
 echo "Analytical query 2"
-jq 'select(."id.resp_p"!=null) ' $FILE >  $typeoffile"geneous_results/analytical2.txt"
+jq -c 'select(."id.resp_p"!=null) ' $FILE >  $typeoffile"geneous_results/analytical2.txt"
 
 echo "Analytical query 3"
-jq -s 'map(.response_body_len) | add/length' $FILE  > $typeoffile"geneous_results/analytical3.txt"
+jq -c -s 'map(.response_body_len) | add/length' $FILE  > $typeoffile"geneous_results/analytical3.txt"
 
 
 
