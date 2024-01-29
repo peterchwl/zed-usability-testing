ce517915fc342b7c89872bdcf9477e018965d460
Siya Kamboj <siyakamboj20@gmail.com>
2024-01-29 14:08:03 -0800

    Changed queries to match what we wrote in group log after meeting professor

--- JSON/Querying/siya/siyaJSONQuerying.sh
+++ JSON/Querying/siya/siyaJSONQuerying.sh
@@ -30,8 +30,8 @@ jq -c 'select(."id.orig_h"=="10.47.5.155")' $FILE > $typeoffile"geneous_results/
 echo "Analytical query 1"
 jq -r '._path' $FILE | sort -u > $typeoffile"geneous_results/analytical1.txt"
 
-echo "Analytical query 2"
-jq -c 'select(."id.resp_p"!=null) ' $FILE >  $typeoffile"geneous_results/analytical2.txt"
+echo "Analytical query 2" 
+cat $FILE | jq -r 'select(.["_path"] != null) | .["_path"]' | sort | uniq -c | awk '{print $2, $1}' > $typeoffile"geneous_results/analytical2.txt"
 
 echo "Analytical query 3"
 jq -c -s 'map(.response_body_len) | add/length' $FILE  > $typeoffile"geneous_results/analytical3.txt"
