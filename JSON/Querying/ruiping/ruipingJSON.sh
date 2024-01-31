b9372bae5f16ec15e97a020732f58fd914d057e3
ruiping-fang <irisfang123@gmail.com>
2024-01-30 17:13:46 -0800

    update

--- JSON/Querying/ruiping/ruipingJSON.sh
+++ JSON/Querying/ruiping/ruipingJSON.sh
@@ -5,17 +5,17 @@ while true; do
   echo "You chose to open $input.ndjson file"
 
   if [ "$input" == "homogeneous" ]; then
-    FILE_PATH="/Users/iris-fang/Documents/code/zed-usability-testing/JSON/Ingesting/ruiping/homogeneous.ndjson"
+    FILE_PATH="../../../JSON/Ingesting/ruiping/homogeneous.ndjson"
     break
   elif [ "$input" == "heterogeneous" ]; then
-    FILE_PATH="/Users/iris-fang/Documents/GitHub/zed-usability-testing/JSON/Querying/ruiping/heterogeneous.ndjson"
+    FILE_PATH="../../../JSON/Ingesting/ruiping/heterogeneous.ndjson"
     break
   else
     echo "Invalid input, please enter 'homogeneous' or 'heterogeneous'"
   fi
 done
 
-FILE_OUTPUT="/Users/iris-fang/Documents/GitHub/zed-usability-testing/JSON/Querying/ruiping"
+FILE_OUTPUT="../../../JSON/Querying/ruiping"
 
 echo "Search Query 1 Done"
 jq 'select(.ts=="2018-03-24T17:15:21.196410Z")' "$FILE_PATH" > $FILE_OUTPUT"/${input}_results/search_query1.txt"
@@ -24,7 +24,7 @@ echo "Search Query 2 Done"
 jq 'select(._path == "http")' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query2.txt"
 
 echo "Search Query 3 Done"
-jq 'select(.status_code == 404)' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query3.txt"
+jq 'select(."id.orig_h"=="10.47.5.155")' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query3.txt"
 
 echo "Analytical Query 1 Done"
 jq -s 'map(._path) | unique' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/analytical_query1.txt"
