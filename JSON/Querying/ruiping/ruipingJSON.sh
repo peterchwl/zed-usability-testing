7417e3c66f3ee78e320a5682818f7b750e33d993
ruiping-fang <irisfang123@gmail.com>
2024-02-01 16:13:37 -0800

    updata SQ3

--- JSON/Querying/ruiping/ruipingJSON.sh
+++ JSON/Querying/ruiping/ruipingJSON.sh
@@ -23,9 +23,11 @@ jq 'select(.ts=="2018-03-24T17:15:21.196410Z")' "$FILE_PATH" > $FILE_OUTPUT"/${i
 echo "Search Query 2 Done"
 jq 'select(._path == "http")' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query2.txt"
 
-echo "Search Query 3 Done"
-jq 'select(."id.orig_h"=="10.47.5.155")' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query3.txt"
+echo "Search Query 4 Done"
+jq 'select(.[]| tostring | contains("10.47.5.155"))' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query3.txt"
 
+#jq 'select(."id.orig_h"=="10.47.5.155")' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/search_query3.txt"
+#id.orig_h == 10.47.5.155
 echo "Analytical Query 1 Done"
 jq -s 'map(._path) | unique' "$FILE_PATH" > "$FILE_OUTPUT/${input}_results/analytical_query1.txt"
 
