031a7d90c209bca39c80409432557e9a5fcf522c
bpanuco11 <brandonpanuco2270@gmail.com>
2024-03-03 22:24:18 -0800

    updated zed/querying/brandon

--- ZED/Querying/brandon/zed-queryingv2.sh
+++ ZED/Querying/brandon/zed-queryingv2.sh
@@ -3,7 +3,7 @@ export ZED_LAKE="C:/ucsd/Winter-2024/CSE 198/Zed/Ingestion/data-ingestion"
 zed use homogeneous_full
 echo "Homogeneous Query Test"
 echo "Search Query #1"
-zed query 'ts==2018-03-24T17:15:21.196410Z' > homogeneous/search1.txt
+zed query -z 'ts==2018-03-24T17:15:21.196410Z' > homogeneous/search1.txt
 
 echo "Search Query #2" 
 #zed query -z '_path=="http"' > homogeneous/search2.txt
@@ -14,7 +14,7 @@ zed query -z '10.47.5.155' > homogeneous/search3.txt
 
 echo "Analytical Query #1" 
 #zed query -z 'by _path | sort' > homogeneous/analytic1.txt
-zed query -z 'typeof(this) | uniq' > homogeneous/analytic1.txt
+zed query -z 'typeof(this) | sort | uniq' > homogeneous/analytic1.txt
 
 echo "Analytical Query #2" 
 #zed query -z 'count(_path) by _path | sort' > homogeneous/analytic2.txt
@@ -37,7 +37,7 @@ zed query -z '10.47.5.155' > heterogeneous/search3.txt
 
 echo "Analytical Query #1" 
 #zed query -z 'by _path | sort' > homogeneous/analytic1.txt
-zed query -z 'typeof(this) | uniq' > heterogeneous/analytic1.txt
+zed query -z 'typeof(this) | sort | uniq' > heterogeneous/analytic1.txt
 
 echo "Analytical Query #2" 
 #zed query -z 'count(_path) by _path | sort' > homogeneous/analytic2.txt
