3d9fd37d4495324ad08dcaca7988f499a73d630b
Siya Kamboj <siyakamboj20@gmail.com>
2024-03-05 16:35:00 -0800

    Data Compression using tar

--- compress_allDataFiles.sh
+++ compress_allDataFiles.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 # Compress DataSet
-folder_to_compress="DataSet"
-zip_file_name="DataSetzipped.zip"
-cd "$(dirname "$folder_to_compress")"
-zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
-echo "Compression complete: $zip_file_name"
+#folder_to_compress="DataSet"
+#zip_file_name="DataSetzipped.zip"
+#cd "$(dirname "$folder_to_compress")"
+#zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
+#echo "Compression complete: $zip_file_name"
+tar -czvf DataSet.tar.gz DataSet
+echo "Compression complete: DataSet"
 
 #Compress JSON Querying
-folder_to_compress="JSON/Querying"
-zip_file_name="JSON-Querying.zip"
-cd "$(dirname "$folder_to_compress")"
-zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
-echo "Compression complete: $zip_file_name"
+# folder_to_compress="JSON/Querying"
+# zip_file_name="JSON-Querying.zip"
+# cd "$(dirname "$folder_to_compress")"
+# zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
+# echo "Compression complete: $zip_file_name"
+cd JSON
+tar -czvf JSON-Querying.tar.gz Querying
+echo "Compression complete: JSON/Querying"
+
 
 #Compress Zed Querying
-folder_to_compress="../Zed"
-zip_file_name="Zed.zip"
-cd "$(dirname "$folder_to_compress")"
-zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
-echo "Compression complete: $zip_file_name"
+# folder_to_compress="../Zed"
+# zip_file_name="Zed.zip"
+# cd "$(dirname "$folder_to_compress")"
+# zip -r "$zip_file_name" "$(basename "$folder_to_compress")"
+# echo "Compression complete: $zip_file_name"
+cd ../
+tar -czvf Zed.tar.gz Zed
+echo "Compression complete: Zed"
 
 #get out of Zed querying and remove all the files that have been zipped
 rm -r DataSet
 rm -r Zed
 rm -r JSON/Querying
+
 
--- uncompress_allDataFiles.sh
+++ uncompress_allDataFiles.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # Set the zip file path and extraction folder
-zip_file="DataSetzipped.zip"
+#zip_file="DataSetzipped.zip"
 #extracted_folder="DataSet"
 # Create the extraction folder if it doesn't exist
 #mkdir -p "$extracted_folder"
 # Unzip the files
 #unzip "$zip_file" -d "$extracted_folder"
-unzip "$zip_file"
+
+
+#unzip "$zip_file"
+tar -xzvf DataSet.tar.gz
 echo "Extraction complete for DataSet"
-rm "$zip_file"
+rm -f DataSet.tar.gz
 
 cd JSON
-zip_file="JSON-Querying.zip"
-unzip "$zip_file"
+#zip_file="JSON-Querying.zip"
+#unzip "$zip_file"
+tar -xzvf JSON-Querying.tar.gz
 echo "Extraction complete for JSON/Querying"
-rm "$zip_file"
+#rm "$zip_file"
+rm -f JSON-Querying.tar.gz
 
 cd ../
-zip_file="Zed.zip"
-unzip "$zip_file"
+#zip_file="Zed.zip"
+#unzip "$zip_file"
+tar -xzvf Zed.tar.gz
 echo "Extraction complete for Zed"
-rm "$zip_file"
-
-
+#rm "$zip_file"
+rm -f Zed.tar.gz
